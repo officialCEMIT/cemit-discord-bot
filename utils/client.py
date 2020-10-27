41b7b3dcf64591b8f8894300f43c49be69e99c8e
millaresluis21@gmail.com <MY_NAME@example.com>
2020-10-27 21:29:09 +0800

    10/27/20

--- utils/client.py
+++ utils/client.py
@@ -32,7 +32,9 @@ async def validate(ctx, user_id):
             cemit.validate_member(user_id)
             role = get(ctx.author.guild.roles, name='MEMBER')
             await ctx.author.add_roles(role)
-            # TODO: REMOVE UNVALIDATED
+            prevRole = get(ctx.author.guild.roles, name='UNVALIDATED')
+            await ctx.author.remove_roles(preVrole)
+            #TODO: REMOVE UNVALIDATED
             await ctx.channel.send("Successfully Validated")
         except MemberExists:
             await ctx.channel.send("The CEMIT member ID you sent is already exists here")
@@ -52,10 +54,11 @@ async def hello(ctx):
 async def on_ready():
     print(f"DISCORD {bot.user.name}(BOT) Ready!")
     bot_channel = get(bot.get_all_channels(), name=CHANNEL_MAP['bot'])
-    await bot_channel.send("Im online!")
+    await bot_channel.send("I'm online!")
 
 
 @bot.event
 async def on_member_join(member):
     role = get(member.guild.roles, name='UNVALIDATED')
     await member.add_roles(role)
+    

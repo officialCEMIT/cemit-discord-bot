08e00e15cd321c565ff3750b600cfd25135b3110
JOSHUA PASCUAL <joshuajohnpascual@gmail.com>
2020-11-30 14:44:30 +0800

    Discord DB Boilerplate

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import discord, pkgutil
+from peewee import SqliteDatabase
 from decouple import config
 from client import bot
 
+
+db = SqliteDatabase('cemit.db')
+
+def startup():
+    db.connect()    
+
+    from services.core.models import setup as core_models
+    #TODO: AUTOMATIC TABLE DETECTION
+    #TODO: Migrations
+    core_models()
+
 if __name__ == "__main__":
+    startup()
     token = config('BOT_TOKEN')
     
     bot.load_extension("services.membership")
